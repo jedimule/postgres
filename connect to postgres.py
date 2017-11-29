584a7528abdc4edf76d1d0185cbea27a4c7cbf53
jedimule <andypoole85@me.com>
2017-11-29 23:01:01 +0000

    Add files via upload

---
+++ connect to postgres.py
@@ -0,0 +1,32 @@
+import psycopg2
+import ogr
+
+host = 'localhost'
+database = 'andy'
+port = '5432'
+user = ''
+password = ''
+
+try:
+    connection = psycopg2.connect(dbname=database, user=user, password=password, host=host, port=port)
+    print connection
+    print 'I can connect'
+
+    cur = connection.cursor()
+
+    cur.execute("select * from test;")
+    result = cur.fetchall()
+    print 'ID' + ' ' + 'Coordinates'
+    for item in result:
+        x = item[2]
+        y = item[3]
+
+        point = ogr.Geometry(ogr.wkbPoint)
+        point.AddPoint(x, y)
+
+        print item[0], point
+
+    cur.close()
+    connection.close()
+except:
+    print 'I can not connect'
