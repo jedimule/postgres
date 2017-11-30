04cac1ba3f95da5e7ef70cd9f1e8b4ce9ff91c31
andy <andypoole85@me.com>
2017-11-30 22:21:52 +0000

    adding ability to create qgis layer from point coordinates

--- connect to postgres.py
+++ connect to postgres.py
@@ -1,8 +1,9 @@
 import psycopg2
-import ogr
 
-#test comment for github
+# layer to save the data too
+pointLayer = QgsVectorLayer('Point?crs=ESPG:4326', 'points', 'memory')
 
+# postgres connection details
 host = 'localhost'
 database = 'andy'
 port = '5432'
@@ -10,25 +11,30 @@ user = 'andy'
 password = ''
 
 try:
+    # opening a connection to the database
     connection = psycopg2.connect(dbname=database, user=user, password=password, host=host, port=port)
-    print connection
     print 'I can connect'
-
     cur = connection.cursor()
-
     cur.execute("select * from test;")
     result = cur.fetchall()
-    print 'ID' + ' ' + 'Coordinates'
+
     for item in result:
         x = item[2]
         y = item[3]
 
-        point = ogr.Geometry(ogr.wkbPoint)
-        point.AddPoint(x, y)
+        # creating geometry from lat and lon values
+        lyr = pointLayer.dataProvider()
+        feat = QgsFeature()
+        feat.setGeometry(QgsGeometry.fromPoint(QgsPoint(x, y)))
 
-        print item[0], point
+        # adding features to the QGS layer
+        lyr.addFeatures([feat])
+        pointLayer.updateExtents()
 
     cur.close()
     connection.close()
 except:
     print 'I can not connect'
+
+# adding layer to the map canvas
+QgsMapLayerRegistry.instance().addMapLayers([pointLayer])
